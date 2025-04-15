33ccbf04163a35f2b75ee3f88c74cdc2c5d0b8c5
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-15 11:42:55 -0400

    added user model

---
+++ src/main/java/com/authentication/authentication/Model/User.java
@@ -0,0 +1,34 @@
+package com.authentication.authentication.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    
+    private LocalDateTime verificationCodeExpiration;
+
+    private boolean enabled;
+}
