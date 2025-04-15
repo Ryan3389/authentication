b16571da9c4379bb4a17e205410369988f3514bf
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-15 12:36:53 -0400

    updated User model

---
+++ src/main/java/com/authentication/authentication/JwtService.java
@@ -0,0 +1,8 @@
+package com.authentication.authentication;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
--- src/main/java/com/authentication/authentication/Model/User.java
+++ src/main/java/com/authentication/authentication/Model/User.java
@@ -3,14 +3,18 @@ package com.authentication.authentication.Model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -27,8 +31,41 @@ public class User {
     @Column(name = "verification_code")
     private String verificationCode;
 
-
+    @Column(name = "verification_expiration")
     private LocalDateTime verificationCodeExpiration;
 
     private boolean enabled;
+
+    // Manage user roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    // Is account still valid (not expired) ?
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // is account locked due to too many failed attempts ?
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+
+    // Checks if user credentials (email, password) are correct
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // Checks if a user is enabled - do they permission to login
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
 }
