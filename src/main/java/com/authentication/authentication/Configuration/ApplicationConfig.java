7f5b88debadaa7760911cbaf02a76b09d9e2c69a
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-16 16:38:30 -0400

    added config files for email and authentication

--- src/main/java/com/authentication/authentication/Configuration/ApplicationConfig.java
+++ src/main/java/com/authentication/authentication/Configuration/ApplicationConfig.java
@@ -5,7 +5,11 @@ import com.authentication.authentication.Repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -16,13 +20,33 @@ public class ApplicationConfig {
         this.userRepository = userRepository;
     }
 
+    // How spring will look up credentials to verify
+    @Bean
+    UserDetailsService userDetailsService() {
+        // Return user email
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+    }
+
+    // Set up password hash for secure PW storage
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Manages user credential auth
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    // Tells us what credentials to look for when verifying
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
 }
+
---
+++ src/main/java/com/authentication/authentication/Configuration/EmailConfig.java
@@ -0,0 +1,35 @@
+package com.authentication.authentication.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class EmailConfig {
+    @Value("${spring.mail.username}")
+    private String email;
+
+    @Value("${spring.mail.password}")
+    private String emailPassword;
+
+    // Set up method to config email send
+    public JavaMailSender javaMailSender(){
+        // Configure email
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(email);
+        mailSender.setPassword(emailPassword);
+
+        // Configure properties
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
