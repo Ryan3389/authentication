3bf98de068dd9793e8998325d3f3398239dd8063
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-16 14:42:06 -0400

    added email send and verify methods

--- src/main/java/com/authentication/authentication/Response/UserResponse.java
+++ src/main/java/com/authentication/authentication/Response/UserResponse.java
@@ -2,11 +2,9 @@ package com.authentication.authentication.Response;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Repository;
 
 @Getter
 @Setter
-@Repository
 public class UserResponse {
     private String token;
     private long expiresIn;
--- src/main/java/com/authentication/authentication/Service/AuthenticationService.java
+++ src/main/java/com/authentication/authentication/Service/AuthenticationService.java
@@ -4,12 +4,15 @@ import com.authentication.authentication.DTO.LoginDTO;
 import com.authentication.authentication.DTO.RegisterUserDTO;
 import com.authentication.authentication.Model.User;
 import com.authentication.authentication.Repository.UserRepository;
+import jakarta.mail.MessagingException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -53,6 +56,76 @@ public class AuthenticationService {
         authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(userInput.getEmail(), userInput.getPassword()));
 
         return user;
+    }
+
+    public void verifyUser(LoginDTO userInput) {
+        Optional<User> optionalUser = userRepository.findByEmail(userInput.getEmail());
+
+        if(optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            if(user.getVerificationCodeExpiration().isBefore(LocalDateTime.now())) {
+                throw new RuntimeException("Please verify your account before logging in");
+            }
+
+            if(user.getVerificationCode().equals(generateVerificationCode())) {
+                user.setEnabled(true);
+                user.setVerificationCode(null);
+                user.setVerificationCodeExpiration(null);
+                userRepository.save(user);
+            }
+            else {
+                throw new RuntimeException("Invalid verification code");
+            }
+
+        } else {
+            throw new RuntimeException("Could not find user with email " + userInput.getEmail());
+        }
+    }
+
+    public void resendVerificationCode(String email) {
+        // Find the user by their email
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        // Set isEnabled to false
+        if(optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if(user.isEnabled()) {
+                throw new RuntimeException("User is already verified");
+            }
+            user.setVerificationCode(generateVerificationCode());
+            user.setVerificationCodeExpiration(LocalDateTime.now().plusMinutes(10));
+            sendVerificationEmail(user);
+            userRepository.save(user);
+
+        } else {
+            throw new RuntimeException("Could not find user with email " + email);
+        }
+    }
+
+
+    public void sendVerificationEmail(User user)  {
+        String emailSubject = "Verify Your Account";
+        String verificationCode = "Verification Code: " + user.getVerificationCode();
+
+        String emailMessage = "<html>" +
+                "<body style=\"font-family: Arial, sans-serif;\">" +
+                    "<div style=\"background-color:#f5f5f5; padding:20px;\">"+
+                        "<h2 style=\"color: #333;>Verify Your Account</h2>\"" +
+                        "<p style=\"font-size: 18px;\">Enter the code below:</p>" +
+                        "<div style=\"background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1); \">" +
+                            "<h3 style=\"color: #333;\">Verification Code: </h3>" +
+                            "<p style=\"font-size: 18px; font-weight: bold; color: #007bff;\">" + verificationCode + "</p>"+
+                        "</div" +
+                    "</div>" +
+                "</body>" +
+                "</html>";
+
+        try {
+            emailService.sendEmail(user.getEmail(), emailSubject, emailMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public String generateVerificationCode() {
