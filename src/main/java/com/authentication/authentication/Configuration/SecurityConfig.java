da7356c04a504e809f41bcb4c0913c335cbb52a9
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-18 12:01:32 -0400

    added security filter

---
+++ src/main/java/com/authentication/authentication/Configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.authentication.authentication.Configuration;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuth jwtAuth;
+
+    public SecurityConfig(AuthenticationProvider authenticationProvider, JwtAuth jwtAuth) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuth = jwtAuth;
+    }
+    // Implement security config
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Disable csrf because we are using JWT (stateless)
+          http.csrf(csrf -> csrf.disable())
+                  .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**")
+                          .permitAll().anyRequest().authenticated()
+                  )
+                // We do not need session authentication, we are using JWT. Set to Stateless
+                  .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                  .authenticationProvider(authenticationProvider)
+                  .addFilterBefore(jwtAuth, UsernamePasswordAuthenticationFilter.class);
+
+          return http.build();
+    }
+}
